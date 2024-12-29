d2ef380691ee193b4a8479ef31c5822db90ee875
Slava Zakariev <124432505+SlavaZakariev@users.noreply.github.com>
2024-12-29 23:15:40 +0300

    Create bucket.tf

---
+++ terraform/bucket/bucket.tf
@@ -0,0 +1,53 @@
+# Создание сервисной УЧ
+resource "yandex_iam_service_account" "sa-terraform" {
+  name = "sa-terraform"
+}
+
+# Выделение прав для сервисной УЧ
+resource "yandex_resourcemanager_folder_iam_member" "terraform-editor" {
+  folder_id  = var.folder_id
+  role       = "editor"
+  member     = "serviceAccount:${yandex_iam_service_account.sa-terraform.id}"
+  depends_on = [ yandex_iam_service_account.sa-terraform ]
+}
+
+# Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa-terraform.id
+  description        = "static access key"
+}
+
+# Использование ключа для бакета
+resource "yandex_storage_bucket" "netology-bucket" {
+  access_key    = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key    = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket        = "zakariev-netology-bucket"
+  acl           = "private"
+  force_destroy = true
+}
+
+# Создать "local_file" для "backendConf"
+resource "local_file" "backendConf" {
+  content  = <<EOT
+endpoint = "storage.yandexcloud.net"
+bucket = "${yandex_storage_bucket.netology-bucket.bucket}"
+region = "ru-central1"
+key = "terraform/terraform.tfstate"
+access_key = "${yandex_iam_service_account_static_access_key.sa-static-key.access_key}"
+secret_key = "${yandex_iam_service_account_static_access_key.sa-static-key.secret_key}"
+skip_region_validation = true
+skip_credentials_validation = true
+EOT
+  filename = "../backend.key"
+}
+
+# Добавить "backendConf" в bucket
+resource "yandex_storage_object" "object-1" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket = yandex_storage_bucket.netology-bucket.bucket
+  key = "terraform.tfstate"
+  source = "./terraform.tfstate"
+  acl    = "private"
+  depends_on = [yandex_storage_bucket.netology-bucket]
+}
