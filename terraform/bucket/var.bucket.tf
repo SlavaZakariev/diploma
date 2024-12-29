32dff94ed2a9427cf5f4fb8b41ce16cdc59a22ce
Slava Zakariev <124432505+SlavaZakariev@users.noreply.github.com>
2024-12-29 23:17:55 +0300

    Create var.bucket.tf

---
+++ terraform/bucket/var.bucket.tf
@@ -0,0 +1,21 @@
+###cloud vars
+variable "token" {
+  type        = string
+  default     = "y0_____"
+  sensitive   = true
+}
+
+variable "cloud_id" {
+  type        = string
+  default     = "b1gq1_____"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1grc_____"
+}
+
+variable "zone_a" {
+  type        = string
+  default     = "ru-central1-a"
+}
